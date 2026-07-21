17dda2a8529cc362ccaf08e5f09cd63d9ddab387
davidson ratis <davidsonmakarios@gmail.com>
2026-07-21 14:05:34 -0300

    feat(api): implement App.setupLoaders

--- api/src/infra/http/index.ts
+++ api/src/infra/http/index.ts
@@ -1,7 +1,20 @@
+import cors from "cors";
 import express, { Application } from "express";
+import helmet from "helmet";
 
 class App {
   private app: Application = express();
+
+  constructor() {
+    this.setupLoaders();
+  }
+
+  private setupLoaders() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(helmet());
+  }
 
   listen(port: number) {
     this.app.listen(port, () => {
