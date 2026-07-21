a0f99ce95b6915436ca193d42840e0c180ac44b3
davidson ratis <davidsonmakarios@gmail.com>
2026-07-21 14:09:12 -0300

    feat(api): implement App.setupErrHandler

--- api/src/infra/http/index.ts
+++ api/src/infra/http/index.ts
@@ -1,12 +1,15 @@
 import cors from "cors";
 import express, { Application } from "express";
 import helmet from "helmet";
+import { CatchAllMiddleware } from "./middlewares/catch-all.middleware";
+import { NotFoundMiddleware } from "./middlewares/not-found.middleware";
 
 class App {
   private app: Application = express();
 
   constructor() {
     this.setupLoaders();
+    this.setupErrHandler();
   }
 
   private setupLoaders() {
@@ -14,6 +17,11 @@ class App {
     this.app.use(express.json());
     this.app.use(cors());
     this.app.use(helmet());
+  }
+
+  private setupErrHandler() {
+    this.app.use(NotFoundMiddleware);
+    this.app.use(CatchAllMiddleware);
   }
 
   listen(port: number) {
