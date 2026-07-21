77b881157928bd012a340e1c8522276d345fb237
davidson ratis <davidsonmakarios@gmail.com>
2026-07-21 10:49:33 -0300

    feat(api) - implement create-user.use-case

---
+++ api/src/use-cases/user/create-user.use-case.ts
@@ -0,0 +1,25 @@
+import { CreateUserInputDto } from "@/dtos/create-user-input.dto";
+import { CreateUserOutputDto } from "@/dtos/create-user-output.dto";
+import { UserMapper } from "@/mappers/user.mapper";
+import { UserRepository } from "@/repositories/user.repository";
+import { ConflictError } from "@/shared/errors/conflict.error";
+import { PasswordHasher } from "./ports/password-hasher";
+
+class CreateUserUseCase {
+  constructor(
+    private userRepository: UserRepository,
+    private passwordHasher: PasswordHasher,
+  ) {}
+
+  async execute({ fullname, email, password }: CreateUserInputDto): Promise<CreateUserOutputDto> {
+    const user = await this.userRepository.getUserByEmail(email);
+    if (user) {
+      throw new ConflictError("User Already exists");
+    }
+    const hash = await this.passwordHasher.hash(password);
+    const newUser = await this.userRepository.create({ fullname, email, password: hash });
+    return UserMapper.toOutput(newUser);
+  }
+}
+
+export { CreateUserUseCase };
