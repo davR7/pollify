4fcccb7c5ad4e726fa6e557e9a9c76983096d970
davidson ratis <davidsonmakarios@gmail.com>
2026-07-22 17:32:04 -0300

    feat(api): register signin route

--- api/src/main/routes/v1/auth.router.ts
+++ api/src/main/routes/v1/auth.router.ts
@@ -1,11 +1,16 @@
 import { Request, Response, Router } from "express";
 import { CreateUserController } from "@/controllers/create-user.controller";
+import { LoginUserController } from "@/controllers/login-user.controller";
 import { container } from "@/infra/container";
 
 const createUser = new CreateUserController(container.getCreateUserUseCase());
+const loginUser = new LoginUserController(container.getLoginUserUseCase());
 
 export default (router: Router) => {
   router.post("/signup", (req: Request, res: Response) => {
     createUser.handler(req, res);
+  });
+  router.post("/signin", (req: Request, res: Response) => {
+    loginUser.handler(req, res);
   });
 };
