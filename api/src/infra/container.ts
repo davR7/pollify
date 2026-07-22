4c2ccc311a2646d1a15f212593a0da05e513093b
davidson ratis <davidsonmakarios@gmail.com>
2026-07-22 17:21:25 -0300

    feat(api): register login-user.use-case in container

--- api/src/infra/container.ts
+++ api/src/infra/container.ts
@@ -1,21 +1,35 @@
 import { CreateUserUseCase } from "@/use-cases/user/create-user.use-case";
 import { UserPrismaRepository } from "./repositories/user-prisma.repository";
 import { BcryptHasher } from "./security/bcrypt.hasher";
+import { LoginUserUseCase } from "@/use-cases/user/login-user.use-case";
+import { JwtTokenProvider } from "./security/jwt-token-provider";
 
 class Container {
   //infra
   private readonly userRepository = new UserPrismaRepository();
   private readonly bcryptHasher = new BcryptHasher();
+  private readonly jwtTokenProvider = new JwtTokenProvider(
+    process.env.SECRET,
+    Number(process.env.JWT_EXPIRES_IN),
+  );
 
   //use-case
   private readonly createUserUseCase = new CreateUserUseCase(
     this.userRepository,
     this.bcryptHasher,
   );
+  private readonly loginUserUseCase = new LoginUserUseCase(
+    this.userRepository,
+    this.bcryptHasher,
+    this.jwtTokenProvider,
+  );
 
   //getter methods
   getCreateUserUseCase() {
     return this.createUserUseCase;
+  }
+  getLoginUserUseCase() {
+    return this.loginUserUseCase;
   }
 }
 
