caa81462be300db2581d22800f91db98bad40c94
davidson ratis <davidsonmakarios@gmail.com>
2026-07-22 11:38:28 -0300

    refactor(api): add router to Express

--- api/src/main/index.ts
+++ api/src/main/index.ts
@@ -1,6 +1,7 @@
 import { App } from "@/infra/http";
+import { router } from "./routes";
 
-const app = new App();
+const app = new App(router);
 const port = Number(process.env.PORT);
 
 app.listen(port);
