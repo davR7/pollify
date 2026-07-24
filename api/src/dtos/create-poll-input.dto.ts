1cb1329342d801bba6d0b7c0de56e63d9d46a6ab
davidson ratis <davidsonmakarios@gmail.com>
2026-07-23 22:16:13 -0300

    feat(api): implement create-poll-input.dto

---
+++ api/src/dtos/create-poll-input.dto.ts
@@ -0,0 +1,11 @@
+import { PollOptionProps } from "@/entities/poll/poll.props";
+import { PollStatus } from "@/entities/poll/poll-status";
+
+export interface CreatePollInputDto {
+  title: string;
+  options: PollOptionProps[];
+  status?: PollStatus;
+  startsAt?: Date;
+  endsAt?: Date;
+  authorId: string;
+}
