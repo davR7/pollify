7848897bed6a24c59b63ad09b8cd243eac95d753
davidson ratis <davidsonmakarios@gmail.com>
2026-07-24 13:41:09 -0300

    feat(api): add list method in poll.repository

--- api/src/repositories/poll.repository.ts
+++ api/src/repositories/poll.repository.ts
@@ -1,6 +1,7 @@
-import { PersistedPollProps, PollProps } from "@/entities/poll/poll.props";
+import { PersistedPoll, PersistedPollProps, PollProps } from "@/entities/poll/poll.props";
 
 export interface PollRepository {
   create(input: PollProps): Promise<PersistedPollProps>;
   getPollById(id: string): Promise<PersistedPollProps | null>;
+  list(): Promise<PersistedPoll[]>;
 }
