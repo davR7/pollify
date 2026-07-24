2992c84b02db67818af28aed378e2259292f7cd9
davidson ratis <davidsonmakarios@gmail.com>
2026-07-23 22:23:05 -0300

    feat(api): implement create-poll.use-case

---
+++ api/src/use-cases/poll/create-poll.use-case.ts
@@ -0,0 +1,19 @@
+import { CreatePollInputDto } from "@/dtos/create-poll-input.dto";
+import { CreatePollOutputDto } from "@/dtos/create-poll-output.dto";
+import { PollStatus } from "@/entities/poll/poll-status";
+import { PollMapper } from "@/mappers/poll.mapper";
+import { PollRepository } from "@/repositories/poll.repository";
+
+class CreatePollUseCase {
+  constructor(private pollRepository: PollRepository) {}
+
+  async execute(input: CreatePollInputDto): Promise<CreatePollOutputDto> {
+    const newPoll = await this.pollRepository.create({
+      ...input,
+      status: PollStatus.DRAFT,
+    });
+    return PollMapper.toOutput(newPoll);
+  }
+}
+
+export { CreatePollUseCase };
