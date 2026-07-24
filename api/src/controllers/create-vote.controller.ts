a7b5ef970d21f6a6422465f5288e243b70e3b852
davidson ratis <davidsonmakarios@gmail.com>
2026-07-24 17:03:45 -0300

    feat(api): implement create-vote.controller

---
+++ api/src/controllers/create-vote.controller.ts
@@ -0,0 +1,22 @@
+import { CreateVoteInputDto } from "@/dtos/create-vote-input.dto";
+import { CreateVoteOutputDto } from "@/dtos/create-vote-output.dto";
+import { CreateVoteParamsDto } from "@/dtos/create-vote-params.dto";
+import { ExpressRequest, ExpressResponse } from "@/infra/http/types";
+import { CreateVoteUseCase } from "@/use-cases/vote/create-vote.use-case";
+
+class CreateVoteController {
+  constructor(private createVoteUseCase: CreateVoteUseCase) {}
+
+  async handler(
+    req: ExpressRequest<CreateVoteInputDto, CreateVoteParamsDto>,
+    res: ExpressResponse<CreateVoteOutputDto>,
+  ) {
+    const body = req.body;
+    const pollId = req.params.pollId;
+    const authorId = res.locals.userId;
+    const output = await this.createVoteUseCase.execute({ ...body, pollId, authorId });
+    return res.status(201).json(output);
+  }
+}
+
+export { CreateVoteController };
