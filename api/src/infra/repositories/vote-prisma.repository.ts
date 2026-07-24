018e13e930af6234e522f1b9b556b389e1d063cf
davidson ratis <davidsonmakarios@gmail.com>
2026-07-24 17:04:10 -0300

    feat(api): implement vote-prisma.repository.ts

---
+++ api/src/infra/repositories/vote-prisma.repository.ts
@@ -0,0 +1,24 @@
+import { PersistedVoteProps, VoteProps } from "@/entities/vote/vote.props";
+import { VoteRepository } from "@/repositories/vote.repository";
+import { prisma } from "../database/prisma";
+
+class VotePrismaRepository implements VoteRepository {
+  async create(input: VoteProps): Promise<PersistedVoteProps> {
+    const newVote = await prisma.vote.create({
+      data: input,
+    });
+    return newVote;
+  }
+
+  async findById(id: string): Promise<PersistedVoteProps | null> {
+    const vote = await prisma.vote.findUnique({ where: { id } });
+    return vote;
+  }
+
+  async findByPollAndUser(pollId: string, authorId: string): Promise<PersistedVoteProps | null> {
+    const vote = await prisma.vote.findFirst({ where: { pollId, authorId } });
+    return vote;
+  }
+}
+
+export { VotePrismaRepository };
