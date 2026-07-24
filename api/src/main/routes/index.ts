c54031901ce3adb77c247612df123b53dd547f39
davidson ratis <davidsonmakarios@gmail.com>
2026-07-24 10:28:11 -0300

    feat(api): register create poll endpoint in router

--- api/src/main/routes/index.ts
+++ api/src/main/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./v1/auth.router";
+import pollRouter from "./v1/poll.router";
 
 const router = Router();
 
 authRouter(router);
+pollRouter(router);
 
 export { router };
