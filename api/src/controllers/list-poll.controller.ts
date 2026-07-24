13bb5bf4bfe306a3490f02abade48c5800c85a9a
davidson ratis <davidsonmakarios@gmail.com>
2026-07-24 13:46:48 -0300

    feat(api): implement list-poll.controller

---
+++ api/src/controllers/list-poll.controller.ts
@@ -0,0 +1,14 @@
+import { ListPollOutputDto } from "@/dtos/list-poll-output.dto";
+import { ExpressRequest, ExpressResponse } from "@/infra/http/types";
+import { ListPollUseCase } from "@/use-cases/poll/list-poll.use-case";
+
+class ListPollController {
+  constructor(private listPollUseCase: ListPollUseCase) {}
+
+  async handler(_req: ExpressRequest<unknown>, res: ExpressResponse<ListPollOutputDto[]>) {
+    const output = await this.listPollUseCase.execute();
+    return res.json(output);
+  }
+}
+
+export { ListPollController };
