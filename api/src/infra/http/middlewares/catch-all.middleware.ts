0b2392a3e0297342e14b124efbc8c9ac54cc1d5e
davidson ratis <davidsonmakarios@gmail.com>
2026-07-26 15:41:02 -0300

    refactor(api): improve error handling

--- api/src/infra/http/middlewares/catch-all.middleware.ts
+++ api/src/infra/http/middlewares/catch-all.middleware.ts
@@ -1,15 +1,33 @@
 import { STATUS_CODES } from "node:http";
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 import { HttpError } from "@/shared/errors/http.error";
 
-function CatchAllMiddleware(err: HttpError, _req: Request, res: Response, _next: NextFunction) {
+function CatchAllMiddleware(err: Error, _req: Request, res: Response, _next: NextFunction) {
   const mode = process.env.NODE_ENV !== "production";
-  const message = mode ? err.message : STATUS_CODES[err.code];
 
-  return res.status(err.code).json({
-    error: err.name,
-    message: message,
-    statusCode: err.code,
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      error: "ValidationFailed",
+      message: err.issues[0].message,
+      statusCode: 400,
+    });
+  }
+
+  if (err instanceof HttpError) {
+    const message = mode ? err.message : STATUS_CODES[err.code];
+
+    return res.status(err.code).json({
+      error: err.name,
+      message: message,
+      statusCode: err.code,
+    });
+  }
+
+  return res.status(500).json({
+    name: "InternalServerError",
+    message: mode ? err.message : "Internal server error",
+    statusCode: 500,
   });
 }
 
