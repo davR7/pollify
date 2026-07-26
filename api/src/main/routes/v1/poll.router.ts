6ead63c0d277becec1a5a10448eb8db8dcc0224a
davidson ratis <davidsonmakarios@gmail.com>
2026-07-25 21:43:14 -0300

    feat(api): register delete poll endpoint in router

--- api/src/main/routes/v1/poll.router.ts
+++ api/src/main/routes/v1/poll.router.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { CreatePollController } from "@/controllers/create-poll.controller";
 import { CreateVoteController } from "@/controllers/create-vote.controller";
+import { DeletePollController } from "@/controllers/delete-poll.controller";
 import { ListPollController } from "@/controllers/list-poll.controller";
 import { container } from "@/infra/container";
 import authMiddleware from "../middlewares/auth.middleware";
@@ -8,6 +9,7 @@ import authMiddleware from "../middlewares/auth.middleware";
 const createPoll = new CreatePollController(container.getCreatePollUseCase());
 const createVote = new CreateVoteController(container.getCreateVoteUseCase());
 const listPoll = new ListPollController(container.getListPollUseCase());
+const delPoll = new DeletePollController(container.getDeletePollUseCase());
 
 export default (router: Router) => {
   router.post("/polls", authMiddleware, (req: Request, res: Response) => {
@@ -18,5 +20,8 @@ export default (router: Router) => {
   });
   router.get("/polls", authMiddleware, (req: Request, res: Response) => {
     listPoll.handler(req, res);
+  });
+  router.delete("/polls/:id", authMiddleware, (req: Request, res: Response) => {
+    delPoll.handler(req, res);
   });
 };
