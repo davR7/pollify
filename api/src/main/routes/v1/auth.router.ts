f041e536150bdea851551a9930fe3f2dcca18132
davidson ratis <davidsonmakarios@gmail.com>
2026-07-26 16:00:45 -0300

    feat(api): register zod-validator.middleware in routes

--- api/src/main/routes/v1/auth.router.ts
+++ api/src/main/routes/v1/auth.router.ts
@@ -2,15 +2,26 @@ import { Request, Response, Router } from "express";
 import { CreateUserController } from "@/controllers/create-user.controller";
 import { LoginUserController } from "@/controllers/login-user.controller";
 import { container } from "@/infra/container";
+import { signinSchema } from "@/schemas/signin.schema";
+import { signupSchema } from "@/schemas/signup.schema";
+import { zodValidatorMiddleware } from "../middlewares/zod-validator.middleware";
 
 const createUser = new CreateUserController(container.getCreateUserUseCase());
 const loginUser = new LoginUserController(container.getLoginUserUseCase());
 
 export default (router: Router) => {
-  router.post("/signup", async (req: Request, res: Response) => {
-    await createUser.handler(req, res);
-  });
-  router.post("/signin", async (req: Request, res: Response) => {
-    await loginUser.handler(req, res);
-  });
+  router.post(
+    "/signup",
+    zodValidatorMiddleware("body", signupSchema),
+    async (req: Request, res: Response) => {
+      await createUser.handler(req, res);
+    },
+  );
+  router.post(
+    "/signin",
+    zodValidatorMiddleware("body", signinSchema),
+    async (req: Request, res: Response) => {
+      await loginUser.handler(req, res);
+    },
+  );
 };
