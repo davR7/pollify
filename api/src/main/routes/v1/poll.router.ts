f166293b299c4beb50262bcb1e95db0401007bb8
davidson ratis <davidsonmakarios@gmail.com>
2026-07-26 22:09:28 -0300

    feat(api): register zod-validator.middleware in routes

--- api/src/main/routes/v1/poll.router.ts
+++ api/src/main/routes/v1/poll.router.ts
@@ -1,11 +1,16 @@
 import { Request, Response, Router } from "express";
 import { CreatePollController } from "@/controllers/create-poll.controller";
 import { CreateVoteController } from "@/controllers/create-vote.controller";
-import { UpdatePollController } from "@/controllers/update-poll.controller";
 import { DeletePollController } from "@/controllers/delete-poll.controller";
 import { ListPollController } from "@/controllers/list-poll.controller";
+import { UpdatePollController } from "@/controllers/update-poll.controller";
 import { container } from "@/infra/container";
+import { createPollSchema } from "@/schemas/create-poll.schema";
+import { deletePollSchema } from "@/schemas/delete-poll.schema";
+import { updatePollSchema } from "@/schemas/update-poll.schema";
+import { votePollSchema } from "@/schemas/vote-poll.schema";
 import authMiddleware from "../middlewares/auth.middleware";
+import { zodValidatorMiddleware } from "../middlewares/zod-validator.middleware";
 
 const createPoll = new CreatePollController(container.getCreatePollUseCase());
 const createVote = new CreateVoteController(container.getCreateVoteUseCase());
@@ -14,19 +19,39 @@ const updatePoll = new UpdatePollController(container.getUpdatePollUseCase());
 const delPoll = new DeletePollController(container.getDeletePollUseCase());
 
 export default (router: Router) => {
-  router.post("/polls", authMiddleware, async (req: Request, res: Response) => {
-    await createPoll.handler(req, res);
-  });
+  router.post(
+    "/polls",
+    authMiddleware,
+    zodValidatorMiddleware("body", createPollSchema),
+    async (req: Request, res: Response) => {
+      await createPoll.handler(req, res);
+    },
+  );
   router.get("/polls", authMiddleware, async (req: Request, res: Response) => {
     await listPoll.handler(req, res);
   });
-  router.patch("/polls/:id", authMiddleware, async (req: Request, res: Response) => {
-    await updatePoll.handler(req, res);
-  });
-  router.delete("/polls/:id", authMiddleware, async (req: Request, res: Response) => {
-    await delPoll.handler(req, res);
-  });
-  router.post("/polls/:pollId/votes", authMiddleware, async (req: Request, res: Response) => {
-    await createVote.handler(req, res);
-  });
+  router.patch(
+    "/polls/:id",
+    authMiddleware,
+    zodValidatorMiddleware("body", updatePollSchema),
+    async (req: Request, res: Response) => {
+      await updatePoll.handler(req, res);
+    },
+  );
+  router.delete(
+    "/polls/:id",
+    authMiddleware,
+    zodValidatorMiddleware("params", deletePollSchema),
+    async (req: Request, res: Response) => {
+      await delPoll.handler(req, res);
+    },
+  );
+  router.post(
+    "/polls/:pollId/votes",
+    authMiddleware,
+    zodValidatorMiddleware("body", votePollSchema),
+    async (req: Request, res: Response) => {
+      await createVote.handler(req, res);
+    },
+  );
 };
