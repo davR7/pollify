f20eac76e25a774a102a83ae3a3f16f9a9c92d6a
davidson ratis <davidsonmakarios@gmail.com>
2026-07-27 18:44:57 -0300

    feat(api): register ensure-admin.middleware in admin route

--- api/src/main/routes/v1/poll.router.ts
+++ api/src/main/routes/v1/poll.router.ts
@@ -5,12 +5,14 @@ import { DeletePollController } from "@/controllers/delete-poll.controller";
 import { ListAvailablePollController } from "@/controllers/list-available-poll.controller";
 import { ListPollController } from "@/controllers/list-poll.controller";
 import { UpdatePollController } from "@/controllers/update-poll.controller";
+import { UserRole } from "@/entities/user/user-role";
 import { container } from "@/infra/container";
 import { createPollSchema } from "@/schemas/create-poll.schema";
 import { deletePollSchema } from "@/schemas/delete-poll.schema";
 import { updatePollSchema } from "@/schemas/update-poll.schema";
 import { votePollSchema } from "@/schemas/vote-poll.schema";
 import authMiddleware from "../middlewares/auth.middleware";
+import ensureAdmin from "../middlewares/ensure-admin.middleware";
 import { zodValidatorMiddleware } from "../middlewares/zod-validator.middleware";
 
 const createPoll = new CreatePollController(container.getCreatePollUseCase());
@@ -32,9 +34,14 @@ export default (router: Router) => {
   router.get("/polls", authMiddleware, async (req: Request, res: Response) => {
     await listAvailablePoll.handler(req, res);
   });
-  router.get("/admin/polls", authMiddleware, async (req: Request, res: Response) => {
-    await listPoll.handler(req, res);
-  });
+  router.get(
+    "/admin/polls",
+    authMiddleware,
+    ensureAdmin([UserRole.ADMIN]),
+    async (req: Request, res: Response) => {
+      await listPoll.handler(req, res);
+    },
+  );
   router.patch(
     "/polls/:id",
     authMiddleware,
