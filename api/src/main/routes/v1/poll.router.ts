f468ffaac77ae36ff26dfeb4b3b7710a9b502a12
davidson ratis <davidsonmakarios@gmail.com>
2026-07-27 15:24:16 -0300

    refactor: use findMany for /polls and findAll for /admin/polls

--- api/src/main/routes/v1/poll.router.ts
+++ api/src/main/routes/v1/poll.router.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { CreatePollController } from "@/controllers/create-poll.controller";
 import { CreateVoteController } from "@/controllers/create-vote.controller";
 import { DeletePollController } from "@/controllers/delete-poll.controller";
+import { ListAvailablePollController } from "@/controllers/list-available-poll.controller";
 import { ListPollController } from "@/controllers/list-poll.controller";
 import { UpdatePollController } from "@/controllers/update-poll.controller";
 import { container } from "@/infra/container";
@@ -15,6 +16,7 @@ import { zodValidatorMiddleware } from "../middlewares/zod-validator.middleware"
 const createPoll = new CreatePollController(container.getCreatePollUseCase());
 const createVote = new CreateVoteController(container.getCreateVoteUseCase());
 const listPoll = new ListPollController(container.getListPollUseCase());
+const listAvailablePoll = new ListAvailablePollController(container.getListAvailablePollUseCase());
 const updatePoll = new UpdatePollController(container.getUpdatePollUseCase());
 const delPoll = new DeletePollController(container.getDeletePollUseCase());
 
@@ -28,6 +30,9 @@ export default (router: Router) => {
     },
   );
   router.get("/polls", authMiddleware, async (req: Request, res: Response) => {
+    await listAvailablePoll.handler(req, res);
+  });
+  router.get("/admin/polls", authMiddleware, async (req: Request, res: Response) => {
     await listPoll.handler(req, res);
   });
   router.patch(
