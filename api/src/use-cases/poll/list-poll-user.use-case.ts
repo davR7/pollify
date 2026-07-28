98c64d2d4e8389477c8eba8d6785f44b3f29b449
davidson ratis <davidsonmakarios@gmail.com>
2026-07-28 11:24:23 -0300

    feat(api): implement list-poll-user.use-case

---
+++ api/src/use-cases/poll/list-poll-user.use-case.ts
@@ -0,0 +1,16 @@
+import { ListPollOutputDto } from "@/dtos/list-poll-output.dto";
+import { ListPollUserInputDto } from "@/dtos/list-poll-user-input.dto";
+import { PollRepository } from "@/repositories/poll.repository";
+
+class ListPollUserUseCase {
+  constructor(private pollRepository: PollRepository) {}
+
+  async execute(input: ListPollUserInputDto): Promise<ListPollOutputDto[]> {
+    const polls = await this.pollRepository.findMany({
+      userId: input.userId,
+    });
+    return polls;
+  }
+}
+
+export { ListPollUserUseCase };
