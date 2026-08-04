ca56f36652d280551f2951ec12e8d7f5f9a0ffd5
davidson ratis <davidsonmakarios@gmail.com>
2026-08-04 18:47:50 -0300

    refactor(api): prefix authentication routes with /auth

--- api/src/main/routes/v1/auth.router.ts
+++ api/src/main/routes/v1/auth.router.ts
@@ -1,33 +1,33 @@
 import { Request, Response, Router } from "express";
-import { CreateUserController } from "@/controllers/create-user.controller";
+import { CreateUserController } from "@/controllers/sign-up.controller";
 import { GetCurrentUserController } from "@/controllers/get-current-user.controller";
-import { LoginUserController } from "@/controllers/login-user.controller";
+import { SignInController } from "@/controllers/sign-in.controller";
 import { container } from "@/infra/container";
 import { signinSchema } from "@/schemas/signin.schema";
 import { signupSchema } from "@/schemas/signup.schema";
 import authMiddleware from "../middlewares/auth.middleware";
 import { zodValidatorMiddleware } from "../middlewares/zod-validator.middleware";
 
-const createUser = new CreateUserController(container.getCreateUserUseCase());
-const loginUser = new LoginUserController(container.getLoginUserUseCase());
+const signUpUseCase = new CreateUserController(container.getSignUpUseCase());
+const signInController = new SignInController(container.getSignInUseCase());
 const currentUser = new GetCurrentUserController(container.getCurrentUserUseCase());
 
 export default (router: Router) => {
   router.post(
-    "/signup",
+    "/auth/signup",
     zodValidatorMiddleware("body", signupSchema),
     async (req: Request, res: Response) => {
-      await createUser.handler(req, res);
+      await signUpUseCase.handler(req, res);
     },
   );
   router.post(
-    "/signin",
+    "/auth/signin",
     zodValidatorMiddleware("body", signinSchema),
     async (req: Request, res: Response) => {
-      await loginUser.handler(req, res);
+      await signInController.handler(req, res);
     },
   );
-  router.get("/me", authMiddleware, async (req: Request, res: Response) => {
+  router.get("/auth/me", authMiddleware, async (req: Request, res: Response) => {
     await currentUser.handler(req, res);
   });
 };
