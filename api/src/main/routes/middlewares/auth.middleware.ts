31c4316901a094eb6782ce61be83da0d78b3dc32
davidson ratis <davidsonmakarios@gmail.com>
2026-08-06 16:18:34 -0300

    refactor: update auth.middleware to support access token

--- api/src/main/routes/middlewares/auth.middleware.ts
+++ api/src/main/routes/middlewares/auth.middleware.ts
@@ -1,35 +1,27 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import { UnauthorizedError } from "@/shared/errors/unauthorized.error";
+import { TokenProvider } from "@/use-cases/auth/ports/token-provider";
 
-export default (req: Request, res: Response, next: NextFunction) => {
-  const authheader = req.headers.authorization;
-  const secret = process.env.SECRET;
+export default function authMiddleware(tokenProvider: TokenProvider) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const authheader = req.headers.authorization;
 
-  if (!authheader) {
-    throw new UnauthorizedError("No token provided");
-  }
-
-  const parts = authheader.split(" ");
-  if (!(parts.length === 2)) {
-    throw new UnauthorizedError("Token Error");
-  }
-
-  const [schema, token] = parts;
-  if (!/^Bearer$/i.test(schema)) {
-    throw new UnauthorizedError("Token malformatted");
-  }
-
-  jwt.verify(token, secret, (error, decoded) => {
-    if (error instanceof TokenExpiredError) {
-      throw new UnauthorizedError("Token expired");
+    if (!authheader) {
+      throw new UnauthorizedError("No access token provided");
     }
 
-    if (error instanceof JsonWebTokenError) {
-      throw new UnauthorizedError("Token invalid");
+    const parts = authheader.split(" ");
+    if (!(parts.length === 2)) {
+      throw new UnauthorizedError("Access token Error");
     }
 
-    res.locals.userId = decoded?.sub;
+    const [schema, accessToken] = parts;
+    if (!/^Bearer$/i.test(schema)) {
+      throw new UnauthorizedError("Access token malformatted");
+    }
+
+    const user = tokenProvider.verifyAccessToken(accessToken);
+    res.locals.userId = user.sub;
     return next();
-  });
-};
+  };
+}
