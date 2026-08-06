02175d639c16ba8f6cb36b8b9db743d508091b3c
davidson ratis <davidsonmakarios@gmail.com>
2026-08-06 16:12:47 -0300

    feat(api): implement auth.constants

---
+++ api/src/shared/constants/auth.constants.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_EXPIRES_IN = 60 * 1;
+export const REFRESH_TOKEN_EXPIRES_IN = 60 * 60 * 24 * 7;
+export const REFRESH_COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
+export const REFRESH_COOKIE_NAME = "jwt";
