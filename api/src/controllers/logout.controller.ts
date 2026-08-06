4e561db492f4c8feff265bfec2c8b1e95528cc3b
davidson ratis <davidsonmakarios@gmail.com>
2026-08-06 16:52:15 -0300

    feat(api): implement logout-token.controller

---
+++ api/src/controllers/logout.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+class LogoutController {
+  async handler(req: Request, res: Response) {
+    const refressToken = req.cookies.jwt;
+    if (!refressToken) {
+      return res.sendStatus(204);
+    }
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
+    res.json({ message: "Logout successful" });
+  }
+}
+
+export { LogoutController };
