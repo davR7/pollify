6ab3a869cdc365fde870003cabce181dfe6b8912
davidson ratis <davidsonmakarios@gmail.com>
2026-08-09 20:35:59 -0300

    feat(web): implement sign-in.schema

---
+++ web/src/schemas/sign-in.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const signinSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Enter a valid email"),
+
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(6, "Password must be at least 6 characters"),
+});
+
+export type SigninFormData = z.infer<typeof signinSchema>;
