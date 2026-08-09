4804069d70ff01809757e34379f13793d9541a28
davidson ratis <davidsonmakarios@gmail.com>
2026-08-09 20:19:54 -0300

    chore(web): change path Header component

--- web/src/layouts/PrivateLayout.tsx
+++ web/src/layouts/PrivateLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import { Footer } from "../components/layout/footer";
+import { Header } from "../components/layout/header";
 
 export function PrivateLayout() {
   return (
