2da0b3438124a3a7a2be2c668dc9895f1554fbc8
davidson ratis <davidsonmakarios@gmail.com>
2026-08-10 17:30:58 -0300

    chore(web): register Toaster sonner in App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 import { router } from "./router";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
+      <Toaster position="top-right" />
       <RouterProvider router={router} />
     </div>
   );
