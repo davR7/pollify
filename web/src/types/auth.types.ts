8dab8e1a5c87ac6624dfde4b7363b12862e871f1
davidson ratis <davidsonmakarios@gmail.com>
2026-08-10 17:32:03 -0300

    feat(web): add types to sign up

--- web/src/types/auth.types.ts
+++ web/src/types/auth.types.ts
@@ -11,6 +11,21 @@ export interface SignInResponse {
   accessToken: string;
 }
 
+export interface SignUpRequest {
+  fullname: string;
+  email: string;
+  password: string;
+}
+
+export interface SignUpResponse {
+  id: string;
+  fullname: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface RefreshTokenResponse {
   accessToken: string;
 }
