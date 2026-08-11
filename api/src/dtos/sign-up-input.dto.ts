c9253d02709573554207fd123ceb1bd083ffafa8
davidson ratis <davidsonmakarios@gmail.com>
2026-08-11 13:59:56 -0300

    refactor(api): remove role property in sign up dtos

--- api/src/dtos/sign-up-input.dto.ts
+++ api/src/dtos/sign-up-input.dto.ts
@@ -1,8 +1,5 @@
-import { UserRole } from "@/entities/user/user-role";
-
 export interface SignUpInputDto {
   fullname: string;
   email: string;
   password: string;
-  role?: UserRole;
 }
--- api/src/dtos/sign-up-output.dto.ts
+++ api/src/dtos/sign-up-output.dto.ts
@@ -1,9 +1,6 @@
-import { UserRole } from "@/entities/user/user-role";
-
 export interface SignUpOutputDto {
   id: string;
   fullname: string;
   email: string;
-  role?: UserRole;
   createdAt: Date;
 }
