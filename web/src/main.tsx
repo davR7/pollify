156c462f115328ad718e778944d09455735decab
davidson ratis <davidsonmakarios@gmail.com>
2026-08-11 14:25:30 -0300

    feat(web): implement QueryClientProvider tanstack react query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import { AuthProvider } from "./contexts/auth/index.tsx";
+import { queryClient } from "./libs/react-query.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
