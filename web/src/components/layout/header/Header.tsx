b24736b8306464a09e6e2669dfa1f21aed03b6a1
davidson ratis <davidsonmakarios@gmail.com>
2026-08-11 11:31:22 -0300

    refactor(web): add Container component in Header

--- web/src/components/layout/header/Header.tsx
+++ web/src/components/layout/header/Header.tsx
@@ -1,12 +1,15 @@
+import { Container } from "@/components/ui/Container";
 import { Menu } from "./components/Menu";
 
 export function Header() {
   return (
-    <header className="border-b border-primary-100">
-      <div className="mx-auto flex h-16 w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <img src="/pollify-logo.png" className="h-14 w-14" alt="Pollify" />
-        <Menu />
-      </div>
+    <header className="border-b border-primary-100 mb-6 py-6">
+      <Container>
+        <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+          <img src="/pollify-logo.png" className="h-14 w-14" alt="Pollify" />
+          <Menu />
+        </div>
+      </Container>
     </header>
   );
 }
