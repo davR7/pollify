4c8c2830b7bf8c900d73e90cb041dddf95e71f4b
davidson ratis <davidsonmakarios@gmail.com>
2026-08-11 14:36:45 -0300

    feat(web): implement MyPollsPage component

--- web/src/pages/polls/my-polls/MyPollsPage.tsx
+++ web/src/pages/polls/my-polls/MyPollsPage.tsx
@@ -1,3 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import { Container } from "@/components/ui/Container";
+import { Grid } from "@/components/ui/Grid";
+import { SectionHeader } from "@/components/ui/SectionHeader";
+import { formatDate } from "@/libs/format-date";
+import { getPollsWithUser } from "@/services/poll.service";
+import { PollCard } from "../components/poll-card";
+
 export function MyPollsPage() {
-  return <main></main>;
+  const { data: output } = useQuery({
+    queryKey: ["pollsWithUser"],
+    queryFn: getPollsWithUser,
+  });
+
+  return (
+    <Container className="flex-1">
+      <SectionHeader title="Suas Enquetes" description="Sua opinião já pode ser compartilhada?" />
+      <Grid>
+        {output?.polls.map((poll) => (
+          <PollCard
+            key={poll.id}
+            id={poll.id}
+            title={poll.title}
+            startsAt={formatDate(poll.startsAt)}
+            endAt={formatDate(poll.endsAt)}
+          />
+        ))}
+      </Grid>
+    </Container>
+  );
 }
