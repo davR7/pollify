1181567efb23d38a1e825f7b30074e0ebfe117b1
davidson ratis <davidsonmakarios@gmail.com>
2026-08-11 11:29:44 -0300

    feat(web): implement Container component

---
+++ web/src/components/ui/Container.tsx
@@ -0,0 +1,13 @@
+import type { HTMLAttributes } from "react";
+
+export function Container({
+  className = "",
+  ...props
+}: HTMLAttributes<HTMLElement>) {
+  return (
+    <main
+      className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}
+      {...props}
+    />
+  );
+}
