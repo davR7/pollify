249b50034bc36c16ff9f9727f1d49f6fa2bd0046
davidson ratis <davidsonmakarios@gmail.com>
2026-08-11 11:31:00 -0300

    refactor(web): add Container component in Footer

--- web/src/components/layout/footer/Footer.tsx
+++ web/src/components/layout/footer/Footer.tsx
@@ -1,20 +1,23 @@
+import { Container } from "@/components/ui/Container";
 import { SocialSection } from "./SocialSection";
 
 export function Footer() {
   return (
     <footer className="border-t border-slate-200 bg-[#3E3835]">
-      <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-between gap-3 px-4 py-6 text-sm text-white sm:flex-row sm:px-6 lg:px-8">
-        <div>
-          <p className="text-base font-semibold">
-            © {new Date().getFullYear()} Pollify. Todos os direitos reservados.
-          </p>
-          <p className="mt-2 max-w-md text-sm leading-6 text-white/75">
-            Criado para tornar mais simples compartilhar perguntas, descobrir opiniões e participar
-            de enquetes com a comunidade.
-          </p>
+      <Container>
+        <div className="flex flex-col items-center justify-between gap-3 text-sm text-white sm:flex-row px-4 py-6  sm:px-6 lg:px-8">
+          <div>
+            <p className="text-base font-semibold">
+              © {new Date().getFullYear()} Pollify. Todos os direitos reservados.
+            </p>
+            <p className="mt-2 max-w-md text-sm leading-6 text-white/75">
+              Criado para tornar mais simples compartilhar perguntas, descobrir opiniões e
+              participar de enquetes com a comunidade.
+            </p>
+          </div>
+          <SocialSection />
         </div>
-        <SocialSection />
-      </div>
+      </Container>
     </footer>
   );
 }
