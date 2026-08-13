1b110d40058ed1430a20961eb6ff9250693dfc87
davidson ratis <davidsonmakarios@gmail.com>
2026-08-13 16:21:49 -0300

    refactor(web): add initialStartDate, formatToInputDateTime and formatToDisplayDateTime

--- web/src/libs/format-date.ts
+++ web/src/libs/format-date.ts
@@ -1,8 +1,14 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-export function formatDate(date: string) {
-  return format(new Date(date), "dd/MM/yyyy 'às' HH:mm", {
-    locale: ptBR,
-  });
+export function initialStartDate(): string {
+  return format(new Date(), "yyyy-MM-dd'T'HH:mm");
+}
+
+export function formatToInputDateTime(date: string): string {
+  return format(new Date(date), "yyyy-MM-dd'T'HH:mm");
+}
+
+export function formatToDisplayDateTime(date: string): string {
+  return format(new Date(date), "dd/MM/yyyy 'às' HH:mm", { locale: ptBR });
 }
