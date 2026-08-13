c738577d1c2d418d1348d60625030183a59f86ee
davidson ratis <davidsonmakarios@gmail.com>
2026-08-13 16:17:34 -0300

    refactor(web): remove user and add status property

--- web/src/models/poll.model.ts
+++ web/src/models/poll.model.ts
@@ -1,12 +1,11 @@
+export type PollStatus = "OPEN" | "CLOSED" | "DRAFT";
+
 export interface Poll {
   id: string;
   title: string;
   options: string[];
+  status: PollStatus;
   startsAt: string;
   endsAt: string;
   createdAt: string;
-  user: {
-    id: string;
-    fullname: string;
-  };
 }
