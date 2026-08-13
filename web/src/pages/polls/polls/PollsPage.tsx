fd9946bf6db686c4b1f9c5e34c88b1850860f639
davidson ratis <davidsonmakarios@gmail.com>
2026-08-13 16:55:20 -0300

    refactor: update Poll for new PollCard integration

--- web/src/pages/polls/polls/PollsPage.tsx
+++ web/src/pages/polls/polls/PollsPage.tsx
@@ -2,14 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { Container } from "@/components/ui/Container";
 import { Grid } from "@/components/ui/Grid";
 import { SectionHeader } from "@/components/ui/SectionHeader";
-import { formatDate } from "@/libs/format-date";
-import { getPolls } from "@/services/poll.service";
+import { getPollsWithUser } from "@/services/poll.service";
 import { PollCard } from "../components/poll-card";
 
 export function PollsPage() {
   const { data: output } = useQuery({
-    queryKey: ["polls"],
-    queryFn: getPolls,
+    queryKey: ["polls", "all"],
+    queryFn: getPollsWithUser,
   });
 
   return (
@@ -17,14 +16,7 @@ export function PollsPage() {
       <SectionHeader title="Enquetes" description="Participe das enquetes da comunidade." />
       <Grid>
         {output?.polls?.map((poll) => (
-          <PollCard
-            key={poll.id}
-            id={poll.id}
-            title={poll.title}
-            author={poll.user.fullname}
-            startsAt={formatDate(poll.startsAt)}
-            endAt={formatDate(poll.endsAt)}
-          />
+          <PollCard key={poll.id} poll={poll} />
         ))}
       </Grid>
     </Container>
