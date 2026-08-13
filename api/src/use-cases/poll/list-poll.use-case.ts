500c7633a310764f95a9b7ad31029cc8b3056165
davidson ratis <davidsonmakarios@gmail.com>
2026-08-13 13:15:46 -0300

    refactor(polls): replace findAll with findMany

--- api/src/use-cases/poll/list-poll.use-case.ts
+++ api/src/use-cases/poll/list-poll.use-case.ts
@@ -5,7 +5,7 @@ class ListPollUseCase {
   constructor(private pollRepository: PollRepository) {}
 
   async execute(): Promise<ListPollOutputDto> {
-    const polls = await this.pollRepository.findAll();
+    const polls = await this.pollRepository.findMany();
     return { polls };
   }
 }
