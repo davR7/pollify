fc5e781be3b578a9e6e710c04bb5674b37e236f3
davidson ratis <davidsonmakarios@gmail.com>
2026-08-13 16:27:54 -0300

    refactor: update poll types and rename methods

--- web/src/services/poll.service.ts
+++ web/src/services/poll.service.ts
@@ -1,13 +1,28 @@
 import { apiPrivate } from "@/libs/api";
-import type { MePollsResponse, PollsResponse } from "@/types/poll.types";
+import type {
+  ListPollResponse,
+  MyPollsResponse,
+  PollStatusPayload,
+  UpdatePollResponse,
+} from "@/types/poll.types";
 
-export async function getPollsWithUser(): Promise<MePollsResponse> {
+export async function getMyPolls(): Promise<MyPollsResponse> {
   const response = await apiPrivate.get("/me/polls");
-  console.log(response.data);
   return response.data;
 }
 
-export async function getPolls(): Promise<PollsResponse> {
+export async function getPollsWithUser(): Promise<ListPollResponse> {
   const response = await apiPrivate.get("/polls");
+  return response.data;
+}
+
+export async function uptadePoll({
+  id,
+  payload,
+}: {
+  id: string;
+  payload: PollStatusPayload;
+}): Promise<UpdatePollResponse> {
+  const response = await apiPrivate.patch(`/polls/${id}`, payload);
   return response.data;
 }
