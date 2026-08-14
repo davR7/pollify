9fdfd7d1958d46657895eed3d3521ca9af44386a
davidson ratis <davidsonmakarios@gmail.com>
2026-08-14 18:24:56 -0300

    refactor(web): handle null values in formatDate utils

--- web/src/libs/format-date.ts
+++ web/src/libs/format-date.ts
@@ -5,10 +5,12 @@ export function initialStartDate(): string {
   return format(new Date(), "yyyy-MM-dd'T'HH:mm");
 }
 
-export function formatToInputDateTime(date: string): string {
+export function formatToInputDateTime(date?: string): string {
+  if (!date) return "—";
   return format(new Date(date), "yyyy-MM-dd'T'HH:mm");
 }
 
-export function formatToDisplayDateTime(date: string): string {
+export function formatToDisplayDateTime(date?: string): string {
+  if (!date) return "—";
   return format(new Date(date), "dd/MM/yyyy 'às' HH:mm", { locale: ptBR });
 }
