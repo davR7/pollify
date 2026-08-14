4241695a732fd6befef54c67daffab0802457e61
davidson ratis <davidsonmakarios@gmail.com>
2026-08-14 18:26:56 -0300

    refactor(web): add PollOption in Poll model

--- web/src/models/poll.model.ts
+++ web/src/models/poll.model.ts
@@ -1,9 +1,16 @@
 export type PollStatus = "OPEN" | "CLOSED" | "DRAFT";
 
+interface PollOption {
+  id: string;
+  text: string;
+  pollId: string;
+  createdAt: string;
+}
+
 export interface Poll {
   id: string;
   title: string;
-  options: string[];
+  options: PollOption[];
   status: PollStatus;
   startsAt: string;
   endsAt: string;
