04d3c51bdf7fc57adc4f15b7b741dd2f3b79a9f0
davidson ratis <davidsonmakarios@gmail.com>
2026-08-18 17:10:19 -0300

    refactor(web): centralize Loading component

--- web/src/components/ui/Loading.tsx
+++ web/src/components/ui/Loading.tsx
@@ -1,8 +1,12 @@
 export function Loading() {
   return (
-    <div className="relative size-12">
-      <div className="absolute inset-0 rounded-full border-8 border-gray-200" />
-      <div className="absolute inset-0 animate-spin rounded-full border-8 border-primary-600 border-t-transparent" />
+    <div className="flex-1">
+      <div className="flex flex-col justify-center items-center">
+        <div className="relative size-12">
+          <div className="absolute inset-0 rounded-full border-8 border-gray-200" />
+          <div className="absolute inset-0 animate-spin rounded-full border-8 border-primary-600 border-t-transparent" />
+        </div>
+      </div>
     </div>
   );
 }
