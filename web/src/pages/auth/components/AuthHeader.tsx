2cfa948908fe4d5d4f612c46cb79746327bf5fce
davidson ratis <davidsonmakarios@gmail.com>
2026-08-18 09:09:40 -0300

    refactor(web): update style AuthHeader component

--- web/src/pages/auth/components/AuthHeader.tsx
+++ web/src/pages/auth/components/AuthHeader.tsx
@@ -1,8 +1,8 @@
 export function AuthHeader({ description }: { description: string }) {
   return (
     <div className="flex flex-col items-center mb-7">
-      <img src="/pollify-logo.png" className="h-52 w-56" alt="Pollify" />
-      <p className="mt-8 mb-2 text-sm leading-6 text-[#8291a8]">{description}</p>
+      <img src="/pollify-logo.png" className="h-52 w-56 mt-5" alt="Pollify" />
+      <p className="mt-8 mb-2 text-sm text-center leading-6 text-slate-400">{description}</p>
     </div>
   );
 }
