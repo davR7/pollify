d69de2463a9239c06fa8dcc82898e93e38472e75
davidson ratis <davidsonmakarios@gmail.com>
2026-08-18 17:11:55 -0300

    refactor(web): implement poll-not-found component

---
+++ web/src/pages/polls/components/poll-not-found/PollNotFound.tsx
@@ -0,0 +1,8 @@
+export function PollNotFound() {
+  return (
+    <div className="py-12 text-center flex-1">
+      <h2 className="text-lg font-semibold text-slate-800">Enquete não encontrada</h2>
+      <p className="mt-2 text-sm text-slate-500">A enquete que você está procurando não existe.</p>
+    </div>
+  );
+}
