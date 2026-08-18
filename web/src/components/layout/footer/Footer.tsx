1e0f753a4d024ed37ef9fa8161aabdd679293fa9
davidson ratis <davidsonmakarios@gmail.com>
2026-08-18 09:18:33 -0300

    refactor(web): update Header and Footer for new navigation

--- web/src/components/layout/footer/Footer.tsx
+++ web/src/components/layout/footer/Footer.tsx
@@ -1,23 +1,25 @@
-import { Container } from "@/components/ui/Container";
+import { Container } from "@/components/layout/container";
 import { SocialSection } from "./SocialSection";
 
 export function Footer() {
   return (
-    <footer className="border-t border-slate-200 bg-[#3E3835]">
-      <Container>
-        <div className="flex flex-col items-center justify-between gap-3 text-sm text-white sm:flex-row px-4 py-6  sm:px-6 lg:px-8">
-          <div>
-            <p className="text-base font-semibold">
-              © {new Date().getFullYear()} Pollify. Todos os direitos reservados.
-            </p>
-            <p className="mt-2 max-w-md text-sm leading-6 text-white/75">
-              Criado para tornar mais simples compartilhar perguntas, descobrir opiniões e
-              participar de enquetes com a comunidade.
-            </p>
+    <div className="pb-20 md:pb-0">
+      <footer className="border-t border-slate-200 bg-[#3E3835] hidden md:block">
+        <Container>
+          <div className="flex flex-col items-center justify-between gap-3 text-sm text-white sm:flex-row px-4 py-6  sm:px-6 lg:px-8">
+            <div>
+              <p className="text-base font-semibold">
+                © {new Date().getFullYear()} Pollify. Todos os direitos reservados.
+              </p>
+              <p className="mt-2 max-w-md text-sm leading-6 text-white/75">
+                Criado para tornar mais simples compartilhar perguntas, descobrir opiniões e
+                participar de enquetes com a comunidade.
+              </p>
+            </div>
+            <SocialSection />
           </div>
-          <SocialSection />
-        </div>
-      </Container>
-    </footer>
+        </Container>
+      </footer>
+    </div>
   );
 }
--- web/src/components/layout/header/Header.tsx
+++ web/src/components/layout/header/Header.tsx
@@ -1,13 +1,13 @@
-import { Container } from "@/components/ui/Container";
-import { Menu } from "./components/Menu";
+import { Container } from "@/components/layout/container";
+import { AppNavigation } from "./components/app-navigation";
 
 export function Header() {
   return (
     <header className="border-b border-primary-100 mb-6 py-6">
       <Container>
-        <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-          <img src="/pollify-logo.png" className="h-14 w-14" alt="Pollify" />
-          <Menu />
+        <div className="flex items-center justify-center md:justify-between h-16 px-4 sm:px-6 lg:px-8">
+          <img src="/pollify-logo.png" className="h-16 w-17 md:h-13 md:w-14" alt="Pollify" />
+          <AppNavigation />
         </div>
       </Container>
     </header>
