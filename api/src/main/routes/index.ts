bf645e9037a95eeb65c3e837d1f210ac966367a6
davidson ratis <davidsonmakarios@gmail.com>
2026-08-19 20:20:12 -0300

    feat(api): register health route in router

--- api/src/main/routes/index.ts
+++ api/src/main/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import healthRouter from "./v1/health.router";
 import authRouter from "./v1/auth.router";
 import pollRouter from "./v1/poll.router";
 
 const router = Router();
 
+healthRouter(router);
 authRouter(router);
 pollRouter(router);
 
