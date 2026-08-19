8c06550263801b4c85087614b337772032927343
davidson ratis <davidsonmakarios@gmail.com>
2026-08-19 09:57:30 -0300

    fix(api): update entry point path

--- api/tsup.config.ts
+++ api/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/main/index.ts"],
   outDir: "dist",
   format: ["cjs"],
   target: "node24",
