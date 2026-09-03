efc5c58781cf31cbfaf16c2725b9c56a82c0c160
davidson ratis <davidsonmakarios@gmail.com>
2026-09-03 20:48:19 -0300

    fix(web): adjust normalizePollDates date conversion

--- web/src/libs/normalizePollDates.ts
+++ web/src/libs/normalizePollDates.ts
@@ -1,11 +1,10 @@
 export function normalizePollDates({ startsAt, endsAt }: { startsAt: string; endsAt: string }) {
   const now = new Date();
 
-  const inputStartsAt = new Date(startsAt);
+  const inputStartsAt = new Date(`${startsAt}T00:00`);
   inputStartsAt.setHours(now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
 
-  const inputEndsAt = new Date(endsAt);
-  inputEndsAt.setHours(23, 59, 0, 0);
+  const inputEndsAt = new Date(`${endsAt}T23:59`);
 
   return { inputStartsAt, inputEndsAt };
 }
